3b6bf8d7b7296a62fec5a650d805eca9d19c2f9a
Argir Argirov <dj.giro@gmail.com>
2017-11-13 16:58:11 -0500

    bonus stop propagation for each other element

--- js/main.js
+++ js/main.js
@@ -14,9 +14,36 @@ document.addEventListener('DOMContentLoaded', function() {
   var submitButton = document.querySelector('.submit')
   var userField = document.querySelector('#user')
   var passField = document.querySelector('#pass')
-  submitButton.addEventListener('click', function() {
+  submitButton.addEventListener('click', function(e) {
     userField.className = 'error'
     passField.className = 'error'
   })
+  userField.addEventListener('focus', function (e) {
+    userField.removeAttribute('class')
+    passField.removeAttribute('class')
+  })
+  passField.addEventListener('focus', function (e) {
+    passField.removeAttribute('class')
+    userField.removeAttribute('class')
+  })
 
+  closeButton.addEventListener('click', function() {
+    modal.style.display = 'none'
+  })
+
+  closeButton.addEventListener('click', function() {
+    modal.style.display = 'none'
+  })
+
+
+  modal.addEventListener('click', function () {
+    modal.style.display = 'none'
+  })
+
+  var elements = [userField, submitButton, passField]
+  elements.forEach(function (element) {
+    element.addEventListener('click', function(e) {
+      e.stopPropagation()
+    })
+  })
 })
